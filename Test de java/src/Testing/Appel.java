94e6472b7aba4b5bf8abd980d42225cf41f94761
erwanFab <erwanFab>
2015-03-09 11:36:18 +0100

    programme de test pour un appel d'une class extérieur
    Signed-off-by: erwanFab <erwanFab>

---
+++ Test de java/src/Testing/Appel.java
@@ -0,0 +1,9 @@
+package Testing;
+
+public class Appel {
+
+	public Appel() {
+		System.out.println("Testons ensemble Eclipse "
+				+ "avec autre class");
+	}
+}
---
+++ Test de java/src/Testing/Testing.java
@@ -0,0 +1,11 @@
+package Testing;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			
+		new Appel();
+	}
+
+}
